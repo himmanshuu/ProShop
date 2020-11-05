91f99299e1685bf829f8ab9713a5337bc7972032
himmanshuu <h.bansaliitg@gmail.com>
2020-11-05 15:55:57 +0530

    Backend Beginning- Data Models and Erorr Handling

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+const app = express();
+dotenv.config();
+
+connectDB();
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_DEVELOPMENT} mode on port ${PORT}`
+      .yellow.bold
+  )
+);
